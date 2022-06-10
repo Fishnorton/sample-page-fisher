b23e2064d36f04a80cd4b29cb3dc05f7a2af39ab
Joshua Ulman <joshuamulman@protonmail.com>
2022-06-09 21:48:32 -0400

    added basic conversion functionality

--- javascript/testJavascript.js
+++ javascript/testJavascript.js
@@ -1,3 +1,48 @@
-function showAlert() {
-    alert('Testing if heroku app only updates on heroku open');
+function convert() {
+
+
+	//Read values in ;)
+	let value1 = document.getElementById("num1").value;
+	let value2 = document.getElementById("num2").value;
+
+
+	//Binary to Decimal
+	if(document.getElementById('binaryToDecimal').checked){
+
+		const answer =  document.getElementById('answer');
+		answer.value = parseInt(value1,2);
+		
+	}
+    
+
+	//Decimal to Binary
+	if(document.getElementById('decimalToBinary').checked){
+
+		const answer =  document.getElementById('answer');
+		answer.value = parseInt(value1).toString(2);
+	}
+	
+
+	//Decimal to Hex
+	if(document.getElementById('decimalToHex').checked){
+
+		const answer =  document.getElementById('answer');
+		answer.value = parseInt(value1).toString(16);
+	}
+
+
+	//Hex to Binary
+	if(document.getElementById('hexToBinary').checked){
+
+		const answer =  document.getElementById('answer');
+		answer.value = parseInt(value1,16).toString(2);
+	}
+
+	//Hex to Decimal
+	if(document.getElementById('hexToDecimal').checked){
+
+		const answer =  document.getElementById('answer');
+		answer.value = parseInt(value1,16);
+	}
+	
 }
